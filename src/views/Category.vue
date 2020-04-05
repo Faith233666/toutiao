3a1600218b2022e68c326fe3f916188e50a3f398
Faith <2955061537@qq.com>
2020-04-05 22:11:01 +0800

    栏目管理加注释

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -4,6 +4,7 @@
        <div class="dellist">
          <p>点击删除以下频道</p>
          <ul class="list" >
+            <!-- includes字符串的包含方法 -->
              <li v-for="(item,index) in addlist" :key="index" :class="['关注','头条'].includes(item.name)?'active':''" 
              @click='addClick(item,index)'>{{item.name}}</li>
          </ul>
@@ -28,25 +29,33 @@ export default {
       dellist:""
     }
   },
+  //页面加载完后触发的事件
   mounted()
   {
+    //获取本地存储的信息
      let categories=JSON.parse(localStorage.getItem('categories'));
      this.categories=categories;
+     //数组筛选方法，提取出is_top==1的
      let addlist=categories.filter(item=>{
        return item.is_top==1;
      })
+      //数组筛选方法，提取出is_top==0的
      let dellist=categories.filter(item=>{
        return item.is_top==0;
      })
+     //将提取出来的数组存入data
      this.addlist=addlist;
      this.dellist=dellist;
   },
+  //页面销毁时触发的事件
   destroyed()
   {
+    //解构赋值
      this.categories=[...this.addlist,...this.dellist,this.categories[this.categories.length-1]];
      localStorage.setItem('categories',JSON.stringify(this.categories));
   },
   methods:{
+    //添加
     addClick(item,index)
     {
       if(item.name=='关注'||item.name=='头条') return;
@@ -55,6 +64,7 @@ export default {
       this.dellist.push(del[0]);
       
     },
+    //删除
     delClick(item,index)
     {
       const del=this.dellist.splice(index,1);
